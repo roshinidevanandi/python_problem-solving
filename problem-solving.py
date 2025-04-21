217e2719c8491312e7afdf1cbfc00e3cde720d29
roshinidevanandi <roshinidevanandi@gmail.com>
2025-04-21 18:28:41 +0530

    yes

---
+++ problem-solving.py
@@ -0,0 +1,9 @@
+def str_fun(str,k):
+    for i in range(len(str)-1,-1,-1):
+        print(str[i],k)
+        print(k)
+
+
+str=input("enter the string here: ")
+num=int(input("enter the number here: "))
+str_fun(str,num)
