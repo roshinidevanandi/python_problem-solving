97fe2a3ef3bbc9082ce338af5355528257b2f755
roshinidevanandi <roshinidevanandi@gmail.com>
2025-04-25 18:11:11 +0530

    yes

---
+++ day1.py
@@ -0,0 +1,12 @@
+# how to work for loop
+
+# fruits=["apple","banana","kiwi"]
+# iter_obj=iter(fruits)
+
+# while True:
+#     try:
+#         fruit=next(iter_obj)
+#         print(fruit)
+#     except StopIteration:
+#         break
+
