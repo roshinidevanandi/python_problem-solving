781b6c2aa270367ff70c57d7c42e5cbd668bdc1d
roshinidevanandi <roshinidevanandi@gmail.com>
2025-05-04 19:44:43 +0530

    yes

--- day3.py
+++ day3.py
@@ -35,25 +35,25 @@
 
 # Example2: class methods and class variables
 
-# class Student:
-#     school="ZPHS SCHOOL"
-#     def __init__(self,m1,m2,m3):
-#         self.m1=m1
-#         self.m2=m2
-#         self.m3=m3
+class Student:
+    school="ZPHS SCHOOL"
+    def __init__(self,m1,m2,m3):
+        self.m1=m1
+        self.m2=m2
+        self.m3=m3
 
-#     def avg(self):
-#         return (self.m1+self.m2+self.m3)/3
+    def avg(self):
+        return (self.m1+self.m2+self.m3)/3
     
-#     @classmethod
-#     def info(cls):
-#         print(cls.school)
+    @classmethod
+    def info(cls):
+        print(cls.school)
 
-# std1=Student(65,86,98)
-# std2=Student(64,82,99)
-# print(std1.avg())
-# print(std2.avg())
-# Student.info()
+std1=Student(65,86,98)
+std2=Student(64,82,99)
+print(std1.avg())
+print(std2.avg())
+Student.info()
 
 
 # Example 3: static method
