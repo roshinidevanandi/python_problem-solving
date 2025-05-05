79ca98665eacb049d7c21df694c79cab80ff59db
roshinidevanandi <roshinidevanandi@gmail.com>
2025-05-05 23:07:21 +0530

    yrs

--- day1.py
+++ day1.py
@@ -79,16 +79,16 @@ except Exception as e:
 
 # 5.factorial
 
-# def factorial(n):
-#     if n==0:
-#         return 1
-#     else:
-#         return n*factorial(n-1)
-# print(factorial(4))
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+print(factorial(4))
 
 # method2 (shortcut)
-# import math
-# print(math.factorial((4)))
+import math
+print(math.factorial((4)))
 
 # Higher order function examples
 # example1:
